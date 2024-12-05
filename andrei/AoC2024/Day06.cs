9746bd24fe7023415ab4fb0f0bfe1dc93f4ec15d
Andrei Lazar <andrei.lazar@centric.eu>
2024-12-05 12:54:13 +0200

    Add my solves for days 1-5

---
+++ andrei/AoC2024/Day06.cs
@@ -0,0 +1,22 @@
+﻿namespace AoC2024
+{
+    internal class Day06
+    {
+        string input;
+        public Day06()
+        {
+            input = InputReader.GetInput(nameof(Day06));
+        }
+
+        public string Part1()
+        {
+            return "TODO";
+        }
+
+
+        public string Part2()
+        {
+            return "TODO";
+        }
+    }
+}
