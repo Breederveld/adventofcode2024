ce804d79f65b8bb3e3b905c56ea2f25128e09586
Chris Breederveld <chris.breederveld@spie.com>
2024-12-05 09:28:10 +0000

    Chris solutions day 5

---
+++ chris/Puzzle.2024-05-1/program.groovy
@@ -0,0 +1,33 @@
+#!/usr/bin/env groovy
+data = new File('input.txt').text;
+lines = data.split("\n");
+rules = [];
+updates = [];
+split = false;
+for (line in lines) {
+	if (line == "") {
+		split = true;
+		continue;
+	}
+	if (split)
+		updates << line.split(",").collect { it.toInteger() };
+	else
+		rules << line.split("\\|").collect { it.toInteger() };
+}
+
+sum = 0;
+for (update in updates) {
+	isValid = true;
+	for (i = 1; i < update.size(); i++) {
+		for (j = 0; j < i; j++) {
+			if (rules.any({ it[0] == update[i] && it[1] == update[j] })) {
+				isValid = false;
+			}
+		}
+	}
+	if (isValid) {
+		sum += update[(int)(update.size() / 2)];
+	}
+}
+
+println sum;
