a2d6e67d86e69dd81d3164e0165d183608828023
Chris Breederveld <chris.breederveld@spie.com>
2024-12-05 09:25:20 +0000

    Chris solutions day 1

---
+++ chris/Puzzle.2024-01-1/Program.vb
@@ -0,0 +1,25 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim line As String
+        Dim numList1 = New List(Of Integer)
+        Dim numList2 = New List(Of Integer)
+        line = Console.ReadLine()
+        Do While Len(line) > 0
+            Dim split = line.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            numList1.Add(Integer.Parse(split(0)))
+            numList2.Add(Integer.Parse(split(1)))
+            line = Console.ReadLine()
+        Loop
+
+        numList1.Sort()
+        numList2.Sort()
+        Dim sum = 0L
+        For i = 0 To numList1.Count - 1
+            sum = sum + Math.Abs(numList2(i) - numList1(i))
+        Next
+
+        Console.WriteLine(sum)
+    End Sub
+End Module
