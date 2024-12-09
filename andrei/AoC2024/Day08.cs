37af9c0e3e2d3fbc603091bbb357f691f4414850
Andrei Lazar <andrei.lazar@centric.eu>
2024-12-09 15:41:42 +0200

    Andrei solves for days 07-09

---
+++ andrei/AoC2024/Day08.cs
@@ -0,0 +1,113 @@
+﻿namespace AoC2024
+{
+    internal class Day08
+    {
+        string input;
+        char[][] map;
+        Dictionary<char, List<(int y, int x)>> antenas = new Dictionary<char, List<(int y, int x)>>();
+        public Day08()
+        {
+            input = InputReader.GetInput(nameof(Day08));
+            map = input.Split("\r\n").Select(x => x.ToCharArray()).ToArray();
+            for (int i = 0; i < map.Length; i++)
+            {
+                for (int j = 0; j < map[i].Length; j++)
+                {
+                    if(map[i][j] != '.')
+                    {
+                        if(!antenas.ContainsKey(map[i][j]))
+                        {
+                            antenas[map[i][j]] = new List<(int y, int x)>() { (i, j) };
+                        }
+                        else
+                        {
+                            antenas[map[i][j]].Add((i, j));
+                        }
+                    }
+                }
+            }
+        }
+
+        public string Part1()
+        {
+            Dictionary<string, bool> antinodes = new Dictionary<string, bool>();
+            foreach (var antena in antenas)
+            {
+                if(antena.Value.Count == 1)
+                {
+                    continue;
+                }
+                for (int i = 0; i < antena.Value.Count-1; i++)
+                {
+                    for (int j = i + 1; j < antena.Value.Count; j++)
+                    {
+                        var a1 = antena.Value[i];
+                        var a2 = antena.Value[j];
+                        var yDif = Math.Abs(a1.y - a2.y);
+                        var xDif = Math.Abs(a1.x - a2.x);
+
+                        var t1y = a1.y + yDif * (a1.y < a2.y ? -1 : 1);
+                        var t1x = a1.x + xDif * (a1.x < a2.x ? -1 : 1);
+
+                        if(t1y>=0 && t1x>=0 && t1y<map.Length && t1x < map[0].Length)
+                        {
+                            antinodes[$"{t1y},{t1x}"] = true;
+                        }
+
+                        var t2y = a2.y + yDif * (a1.y < a2.y ? 1 : -1);
+                        var t2x = a2.x + xDif * (a1.x < a2.x ? 1 : -1);
+                        if(t2y >= 0 && t2x >= 0 && t2y < map.Length && t2x < map[0].Length)
+                        {
+                            antinodes[$"{t2y},{t2x}"] = true;
+                        }
+                    }
+                }
+            }
+            return antinodes.Count.ToString();
+        }
+
+
+        public string Part2()
+        {
+            var antinodes = new Dictionary<string, bool>();
+            foreach(var antena in antenas)
+            {
+                if (antena.Value.Count == 1)
+                {
+                    continue;
+                }
+                antena.Value.ForEach(x => antinodes[$"{x.y},{x.x}"] = true);
+                for (int i = 0; i < antena.Value.Count - 1; i++)
+                {
+                    for (int j = i + 1; j < antena.Value.Count; j++)
+                    {
+                        var a1 = antena.Value[i];
+                        var a2 = antena.Value[j];
+                        var yDif = Math.Abs(a1.y - a2.y);
+                        var xDif = Math.Abs(a1.x - a2.x);
+
+                        var t1y = a1.y + yDif * (a1.y < a2.y ? -1 : 1);
+                        var t1x = a1.x + xDif * (a1.x < a2.x ? -1 : 1);
+
+                        while(t1y >= 0 && t1x >= 0 && t1y < map.Length && t1x < map[0].Length)
+                        {
+                            antinodes[$"{t1y},{t1x}"] = true;
+                            t1y += yDif * (a1.y < a2.y ? -1 : 1);
+                            t1x += xDif * (a1.x < a2.x ? -1 : 1);
+                        }
+
+                        var t2y = a2.y + yDif * (a1.y < a2.y ? 1 : -1);
+                        var t2x = a2.x + xDif * (a1.x < a2.x ? 1 : -1);
+                        while(t2y >= 0 && t2x >= 0 && t2y < map.Length && t2x < map[0].Length)
+                        {
+                            antinodes[$"{t2y},{t2x}"] = true;
+                            t2y += yDif * (a1.y < a2.y ? 1 : -1);
+                            t2x += xDif * (a1.x < a2.x ? 1 : -1);
+                        }
+                    }
+                }
+            }
+            return antinodes.Count.ToString();
+        }
+    }
+}
---
+++ andrei/AoC2024/Day09.cs
@@ -0,0 +1,141 @@
+﻿using System.Numerics;
+
+namespace AoC2024
+{
+    internal class Day09
+    {
+        string input;
+        public Day09()
+        {
+            input = InputReader.GetInput(nameof(Day09));
+        }
+
+        public string Part1()
+        {
+            List<int> disk = BuildDisk();
+
+            var blank = disk.IndexOf(-1);
+            var cursor = disk.Count - 1;
+            while(cursor > blank)
+            {
+                if (disk[cursor] == -1)
+                {
+                    cursor--;
+                    continue;
+                }
+                else 
+                {
+                    disk[blank] = disk[cursor];
+                    disk[cursor] = -1;
+                    cursor--;
+                    while (disk[blank] != -1)
+                    {
+                        blank++;
+                    }
+                }
+            }
+
+            long checkSum = GetChecksum(disk);
+            return checkSum.ToString();
+        }
+
+
+        public string Part2()
+        {
+            var disk = BuildDisk();
+            var blank = disk.IndexOf(-1);
+            var cursor = disk.Count - 1;
+            while (cursor > blank)
+            {
+                if (disk[cursor] == -1)
+                {
+                    cursor--;
+                    continue;
+                }
+
+                var fileSize = 1;
+
+                var fileCursor = cursor;
+                while (disk[fileCursor-1] == disk[cursor])
+                {
+                    fileSize++;
+                    fileCursor--;
+                }
+
+                // find first sequence of -1s at least as long as the fileSize
+                var blankSpace = 1;
+                var blankStart = blank;
+                while (blankSpace < fileSize && blank < fileCursor)
+                {
+                    blank++;
+                    if (disk[blank] == -1)
+                    {
+                        blankSpace++;
+                    }
+                    else
+                    {
+                        while (blank < fileCursor && disk[blank] != -1)
+                        {
+                            blank++;
+                        }
+                        blankSpace = 1;
+                        blankStart = blank;
+                    }
+                }
+
+                if (blankSpace == fileSize)
+                {
+                    for (int i = 0; i < fileSize; i++)
+                    {
+                        disk[blankStart + i] = disk[fileCursor+i];
+                        disk[fileCursor+i] = -1;
+                    }
+                }
+
+                cursor = fileCursor - 1;
+                blank = disk.IndexOf(-1);
+            }
+            return GetChecksum(disk).ToString();
+        }
+
+        private List<int> BuildDisk()
+        {
+            List<int> disk = new List<int>();
+            var currentId = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                var current = input[i] - '0';
+                for (int j = 0; j < current; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        disk.Add(currentId);
+                    }
+                    else
+                    {
+                        disk.Add(-1);
+                    }
+                }
+                if (i % 2 == 0)
+                {
+                    currentId++;
+                }
+            }
+            return disk;
+        }
+
+        private long GetChecksum(List<int> disk)
+        {
+            long checkSum = 0;
+            for (int i = 0; i < disk.Count; i++)
+            {
+                if (disk[i] == -1)
+                {
+                    continue;
+                }
+                checkSum += disk[i] * i;
+            }
+            return checkSum;
+        }
+    }
+}
